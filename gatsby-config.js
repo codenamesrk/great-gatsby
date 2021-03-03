afcd59cabf03a172289c9fc098a51ccb136ae317
Srijit RK <srijit@outlook.com>
2021-03-03 23:15:42 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: "Great Gatsby",
+  },
+  plugins: [
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
